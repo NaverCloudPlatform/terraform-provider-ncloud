9dc206cf4411a077957f5fe08b8d44517cdbfb72
modern.lim <modern.lim@navercorp.com>
2018-05-28 13:18:42 +0900

    Implement terminateServerInstance

--- ncloud/common.go
+++ ncloud/common.go
@@ -1,6 +1,7 @@
 package ncloud
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/NaverCloudPlatform/ncloud-sdk-go/common"
 	"log"
@@ -14,6 +15,13 @@ func convertToStringList(configured []interface{}) []string {
 	return vs
 }
 
-func logCommonResponse(tag string, resp common.CommonResponse) {
-	log.Printf("[DEBUG] %s Response: %s", tag, fmt.Sprintf("RequestID: %s, ReturnCode: %d, ReturnMessage: %s", resp.RequestID, resp.ReturnCode, resp.ReturnMessage))
+func logCommonResponse(tag string, err error, args interface{}, resp common.CommonResponse) {
+	param, _ := json.Marshal(args)
+
+	if err != nil {
+		log.Printf("[DEBUG] %s error params=%s, err=%s", tag, param, err)
+	} else {
+		result := fmt.Sprintf("RequestID: %s, ReturnCode: %d, ReturnMessage: %s", resp.RequestID, resp.ReturnCode, resp.ReturnMessage)
+		log.Printf("[DEBUG] %s success params=%s, response=%s", tag, param, result)
+	}
 }
--- ncloud/resource_ncloud_instance.go
+++ ncloud/resource_ncloud_instance.go
@@ -8,18 +8,22 @@ import (
 )
 
 // Interval for checking status in WaitForXXX method
-const DefaultWaitForInterval = 5
+const DefaultWaitForInterval = 10
 
 // Default timeout
 const DefaultTimeout = 60
 const DefaultCreateTimeout = 15 * 60
+const DefaultStopTimeout = 5 * 60
 
 func resourceNcloudInstance() *schema.Resource {
 	return &schema.Resource{
 		Create: resourceNcloudInstanceCreate,
 		Read:   resourceNcloudInstanceRead,
 		Delete: resourceNcloudInstanceDelete,
-		//Update: resourceNcloudInstanceUpdate,
+		Update: resourceNcloudInstanceUpdate,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
 
 		Timeouts: &schema.ResourceTimeout{
 			Create: schema.DefaultTimeout(DefaultCreateTimeout),
@@ -34,7 +38,6 @@ func resourceNcloudInstance() *schema.Resource {
 			"server_product_code": {
 				Type:     schema.TypeString,
 				Optional: true,
-				ForceNew: true,
 			},
 			"member_server_image_no": {
 				Type:     schema.TypeString,
@@ -114,19 +117,21 @@ func resourceNcloudInstanceCreate(d *schema.ResourceData, meta interface{}) erro
 	if err != nil {
 		return err
 	}
-	logCommonResponse("CreateServerInstances", resp.CommonResponse)
+	logCommonResponse("CreateServerInstances", err, reqParams, resp.CommonResponse)
 
 	serverInstance := &resp.ServerInstanceList[0]
 	d.SetId(serverInstance.ServerInstanceNo)
 
-	if err := WaitForInstance(meta, serverInstance.ServerInstanceNo, "RUN", DefaultCreateTimeout); err != nil {
+	if err := WaitForInstance(conn, serverInstance.ServerInstanceNo, "RUN", DefaultCreateTimeout); err != nil {
 		return err
 	}
-	return nil
+	return resourceNcloudInstanceRead(d, meta)
 }
 
 func resourceNcloudInstanceRead(d *schema.ResourceData, meta interface{}) error {
-	serverInstance, err := getServerInstance(meta, d.Id())
+	conn := meta.(*NcloudSdk).conn
+
+	serverInstance, err := getServerInstance(conn, d.Id())
 	if err != nil {
 		return err
 	}
@@ -135,6 +140,13 @@ func resourceNcloudInstanceRead(d *schema.ResourceData, meta interface{}) error 
 		// TODO:
 		d.Set("server_name", serverInstance.ServerName)
 		d.Set("server_image_product_code", serverInstance.ServerImageProductCode)
+		d.Set("server_instance_status_name", serverInstance.ServerInstanceStatusName)
+		d.Set("uptime", serverInstance.Uptime)
+		d.Set("server_image_name", serverInstance.ServerImageName)
+		d.Set("private_ip", serverInstance.PrivateIP)
+		d.Set("cpu_count", serverInstance.CPUCount)
+		d.Set("memory_size", serverInstance.MemorySize)
+		d.Set("base_block_storage_size", serverInstance.BaseBlockStorageSize)
 	}
 
 	return nil
@@ -142,23 +154,119 @@ func resourceNcloudInstanceRead(d *schema.ResourceData, meta interface{}) error 
 
 func resourceNcloudInstanceDelete(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*NcloudSdk).conn
-
-	reqParams := &sdk.RequestTerminateServerInstances{
-		ServerInstanceNoList: []string{d.Id()},
+	serverInstance, err := getServerInstance(conn, d.Id())
+	if err != nil {
+		return err
 	}
-	if _, err := conn.TerminateServerInstances(reqParams); err != nil {
+
+	if serverInstance.ServerInstanceStatus.Code != "NSTOP" {
+		stopServerInstance(conn, d.Id())
+		if err := WaitForInstance(conn, serverInstance.ServerInstanceNo, "NSTOP", DefaultStopTimeout); err != nil {
+			return err
+		}
+	}
+
+	return terminateServerIntance(conn, d.Id())
+}
+
+func resourceNcloudInstanceUpdate(d *schema.ResourceData, meta interface{}) error {
+	//conn := meta.(*NcloudSdk).conn
+
+	if d.HasChange("server_product_code") {
+		// TODO: Implement ChangeServerInstanceSpec API
+		/*
+			reqParams := &sdk.RequestChangeServerInstanceSpec{
+				ServerInstanceNo: d.Get("server_instance_no"),
+				ServerProductCode: d.Get("server_product_code")
+			}
+
+			resp, err := conn.ChangeServerInstanceSpec(reqParams)
+			logCommonResponse("ChangeServerInstanceSpec", err, reqParams, resp.CommonResponse)
+
+			if err != nil {
+				return err
+			}
+		*/
+	}
+
+	return resourceNcloudInstanceRead(d, meta)
+}
+
+func buildCreateServerInstanceReqParams(d *schema.ResourceData) *sdk.RequestCreateServerInstance {
+
+	var paramAccessControlGroupConfigurationNoList []string
+	if param, ok := d.GetOk("access_control_group_configuration_no_list"); ok {
+		paramAccessControlGroupConfigurationNoList = convertToStringList(param.(*schema.Set).List())
+	}
+
+	reqParams := &sdk.RequestCreateServerInstance{
+		ServerImageProductCode:                d.Get("server_image_product_code").(string),
+		ServerProductCode:                     d.Get("server_product_code").(string),
+		MemberServerImageNo:                   d.Get("member_server_image_no").(string),
+		ServerName:                            d.Get("server_name").(string),
+		ServerDescription:                     d.Get("server_description").(string),
+		LoginKeyName:                          d.Get("login_key_name").(string),
+		IsProtectServerTermination:            d.Get("is_protect_server_termination").(bool),
+		ServerCreateCount:                     d.Get("server_create_count").(int),
+		ServerCreateStartNo:                   d.Get("server_create_start_no").(int),
+		InternetLineTypeCode:                  d.Get("internet_line_type_code").(string),
+		FeeSystemTypeCode:                     d.Get("fee_system_type_code").(string),
+		ZoneNo:                                d.Get("zone_no").(string),
+		AccessControlGroupConfigurationNoList: paramAccessControlGroupConfigurationNoList,
+		UserData:                              d.Get("user_data").(string),
+	}
+	return reqParams
+}
+
+func getServerInstance(conn *sdk.Conn, serverInstanceNo string) (*sdk.ServerInstance, error) {
+	reqParams := new(sdk.RequestGetServerInstanceList)
+	reqParams.ServerInstanceNoList = []string{serverInstanceNo}
+	resp, err := conn.GetServerInstanceList(reqParams)
+
+	if err != nil {
+		return nil, err
+	}
+	logCommonResponse("GetServerInstanceList", err, reqParams, resp.CommonResponse)
+
+	if len(resp.ServerInstanceList) > 0 {
+		return &resp.ServerInstanceList[0], nil
+	}
+	return nil, nil
+}
+
+func stopServerInstance(conn *sdk.Conn, serverInstanceNo string) error {
+	reqParams := &sdk.RequestStopServerInstances{
+		ServerInstanceNoList: []string{serverInstanceNo},
+	}
+	resp, err := conn.StopServerInstances(reqParams)
+	logCommonResponse("StopServerInstances", err, reqParams, resp.CommonResponse)
+
+	if err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func WaitForInstance(meta interface{}, instanceId string, status string, timeout int) error {
+func terminateServerIntance(conn *sdk.Conn, serverInstanceNo string) error {
+	reqParams := &sdk.RequestTerminateServerInstances{
+		ServerInstanceNoList: []string{serverInstanceNo},
+	}
+	resp, err := conn.TerminateServerInstances(reqParams)
+	logCommonResponse("TerminateServerInstances", err, reqParams, resp.CommonResponse)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func WaitForInstance(conn *sdk.Conn, instanceId string, status string, timeout int) error {
 	if timeout <= 0 {
 		timeout = DefaultWaitForInterval
 	}
 	for {
-		instance, err := getServerInstance(meta, instanceId)
+		instance, err := getServerInstance(conn, instanceId)
 		if err != nil {
 			return err
 		}
@@ -175,48 +283,4 @@ func WaitForInstance(meta interface{}, instanceId string, status string, timeout
 		time.Sleep(DefaultWaitForInterval * time.Second)
 	}
 	return nil
-}
-
-func buildCreateServerInstanceReqParams(d *schema.ResourceData) *sdk.RequestCreateServerInstance {
-
-	var paramAccessControlGroupConfigurationNoList []string
-	if param, ok := d.GetOk("access_control_group_configuration_no_list"); ok {
-		paramAccessControlGroupConfigurationNoList = convertToStringList(param.(*schema.Set).List())
-	}
-
-	reqParams := &sdk.RequestCreateServerInstance{
-		ServerImageProductCode:     d.Get("server_image_product_code").(string),
-		ServerProductCode:          d.Get("server_product_code").(string),
-		MemberServerImageNo:        d.Get("member_server_image_no").(string),
-		ServerName:                 d.Get("server_name").(string),
-		ServerDescription:          d.Get("server_description").(string),
-		LoginKeyName:               d.Get("login_key_name").(string),
-		IsProtectServerTermination: d.Get("is_protect_server_termination").(bool),
-		ServerCreateCount:          d.Get("server_create_count").(int),
-		ServerCreateStartNo:        d.Get("server_create_start_no").(int),
-		InternetLineTypeCode:       d.Get("internet_line_type_code").(string),
-		FeeSystemTypeCode:          d.Get("fee_system_type_code").(string),
-		ZoneNo:                     d.Get("zone_no").(string),
-		AccessControlGroupConfigurationNoList: paramAccessControlGroupConfigurationNoList,
-		UserData: d.Get("user_data").(string),
-	}
-	return reqParams
-}
-
-func getServerInstance(meta interface{}, serverInstanceNo string) (*sdk.ServerInstance, error) {
-	conn := meta.(*NcloudSdk).conn
-	reqParams := new(sdk.RequestGetServerInstanceList)
-	reqParams.ServerInstanceNoList = []string{serverInstanceNo}
-	resp, err := conn.GetServerInstanceList(reqParams)
-
-	if err != nil {
-		return nil, err
-	}
-	if resp == nil {
-		return nil, fmt.Errorf("error: GetServerInstanceList response error")
-	}
-	if len(resp.ServerInstanceList) > 0 {
-		return &resp.ServerInstanceList[0], nil
-	}
-	return nil, nil
 }
