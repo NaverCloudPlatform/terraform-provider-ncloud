0fdf11bfa37eaab9b069ab776a70e5b9ba427fd6
modern.lim <modern.lim@navercorp.com>
2018-06-11 17:02:55 +0900

    add ansible example

---
+++ examples/ansible/main.tf
@@ -0,0 +1,47 @@
+provider "ncloud" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region = "${var.region}"
+}
+
+resource "ncloud_login_key" "key" {
+  "key_name" = "${var.login_key_name}"
+}
+
+resource "ncloud_instance" "instance" {
+  "server_name" = "${var.server_name}"
+  "server_image_product_code" = "${var.server_image_product_code}"
+  "server_product_code" = "${var.server_product_code}"
+  "login_key_name" = "${ncloud_login_key.key.key_name}"
+}
+
+data "ncloud_root_password" "rootpwd" {
+  "server_instance_no" = "${ncloud_instance.instance.id}"
+  "private_key" = "${ncloud_login_key.key.private_key}"
+}
+
+data "ncloud_port_forwarding_rules" "rules" {}
+
+resource "ncloud_port_forwarding_rule" "forwarding" {
+  "port_forwarding_configuration_no" = "${data.ncloud_port_forwarding_rules.rules.id}"
+  "server_instance_no" = "${ncloud_instance.instance.id}"
+  "port_forwarding_external_port" = "${var.port_forwarding_external_port}"
+  "port_forwarding_internal_port" = "22"
+}
+
+resource "null_resource" "ssh" {
+
+  provisioner "local-exec" {
+    command = <<EOF
+      echo "[demo]" > inventory
+      echo "${ncloud_port_forwarding_rule.forwarding.port_forwarding_public_ip} ansible_ssh_user=root ansible_ssh_pass='${data.ncloud_root_password.rootpwd.root_password}'"
+    EOF
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      ANSIBLE_HOST_KEY_CHECKING=False \
+      ansible-playbook -i inventory playbook.yml
+    EOF
+  }
+}
---
+++ examples/ansible/variables.tf
@@ -0,0 +1,26 @@
+variable "access_key" {} # export TF_VAR_access_key=...
+variable "secret_key" {} # export TF_VAR_secret_key=...
+
+variable "region" {
+  default = "KR"
+}
+
+variable "login_key_name" {
+  default = "tf-ansibletest-key"
+}
+
+variable "server_name" {
+  default = "tf-ansibletest-vm"
+}
+
+variable "server_image_product_code" {
+  default = "SPSW0LINUX000032"
+}
+
+variable "server_product_code" {
+  default = "SPSVRSTAND000004" #SPSVRSTAND000056
+}
+
+variable "port_forwarding_external_port" {
+  default = "5088"
+}
