7368c59d5ee10c4bf4de3b0e14a68be7414c5f12
유성덕 <sungduk.yu@navercorp.com>
2018-06-15 19:21:27 +0900

    implement config Nas volume instance access control

--- examples/nas_volume/main.tf
+++ examples/nas_volume/main.tf
@@ -1,11 +1,11 @@
 provider "ncloud" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
-  region = "${var.region}"
+  region     = "${var.region}"
 }
 
 resource "ncloud_nas_volume" "nas" {
-  "volume_name_postfix" = "tftest_vol"
-  "volume_size_gb" = "500"
+  "volume_name_postfix"                 = "tftest_vol"
+  "volume_size_gb"                      = "500"
   "volume_allotment_protocol_type_code" = "NFS"
 }
