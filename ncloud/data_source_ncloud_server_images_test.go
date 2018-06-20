80f7503c8dbc2d40ac87dbab787442bb5f3c3883
modern.lim <modern.lim@navercorp.com>
2018-06-20 13:51:15 +0900

    add infra_resource_detail_type_code

--- ncloud/data_source_ncloud_server_images_test.go
+++ ncloud/data_source_ncloud_server_images_test.go
@@ -17,7 +17,58 @@ func TestAccDataSourceNcloudServerImagesBasic(t *testing.T) {
 			{
 				Config: testAccDataSourceNcloudServerImagesConfig,
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckNcloudServerImagesDataSourceID("data.ncloud_server_images.server_images"),
+					testAccCheckNcloudServerImagesDataSourceID("data.ncloud_server_images.test"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDataSourceNcloudServerImagesLinux(t *testing.T) {
+	t.Parallel()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceNcloudServerImagesLinuxConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNcloudServerImagesDataSourceID("data.ncloud_server_images.linux"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDataSourceNcloudServerImagesWindows(t *testing.T) {
+	t.Parallel()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceNcloudServerImagesWindowsConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNcloudServerImagesDataSourceID("data.ncloud_server_images.windows"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDataSourceNcloudServerImagesBareMetal(t *testing.T) {
+	t.Parallel()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceNcloudServerImagesBareMetalConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNcloudServerImagesDataSourceID("data.ncloud_server_images.beremetal"),
 				),
 			},
 		},
@@ -39,5 +90,23 @@ func testAccCheckNcloudServerImagesDataSourceID(n string) resource.TestCheckFunc
 }
 
 var testAccDataSourceNcloudServerImagesConfig = `
-data "ncloud_server_images" "server_images" {}
+data "ncloud_server_images" "test" {}
+`
+
+var testAccDataSourceNcloudServerImagesLinuxConfig = `
+data "ncloud_server_images" "linux" {
+	"platform_type_code_list" = ["LNX32", "LNX64"]
+}
+`
+
+var testAccDataSourceNcloudServerImagesWindowsConfig = `
+data "ncloud_server_images" "windows" {
+	"platform_type_code_list" = ["WND32", "WND64"]
+}
+`
+
+var testAccDataSourceNcloudServerImagesBareMetalConfig = `
+data "ncloud_server_images" "beremetal" {
+	"infra_resource_detail_type_code" = "BM"
+}
 `
