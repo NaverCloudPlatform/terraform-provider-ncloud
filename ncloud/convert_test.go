c13798adf8d0dcc1855c02d236294486a612f612
modern.lim <modern.lim@navercorp.com>
2018-06-21 10:43:23 +0900

    fix test error

--- ncloud/convert_test.go
+++ ncloud/convert_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestBase64EncodeDecode(t *testing.T) {
 	data := `CreateObject("WScript.Shell").run("cmd.exe /c powershell Set-ExecutionPolicy RemoteSigned & winrm set winrm/config/service/auth @{Basic="true"} & winrm set winrm/config/service @{AllowUnencrypted="true"} & winrm quickconfig -q & sc config WinRM start= auto & winrm get winrm/config/service")`
-	encoded := Base64Decode(data)
+	encoded := Base64Encode(data)
 	fmt.Printf("encoded: %s\n", encoded)
 
 	decoded := Base64Decode(encoded)
