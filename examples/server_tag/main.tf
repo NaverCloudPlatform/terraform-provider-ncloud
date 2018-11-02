28b6f55c16990138a2cb87ea641f5226c3c704b4
유성덕 <sungduk.yu@navercorp.com>
2018-11-02 17:27:43 +0900

    Add server instance tag sample

---
+++ examples/server_tag/main.tf
@@ -0,0 +1,25 @@
+provider "ncloud" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+}
+
+data "ncloud_zones" "korea" {
+  "region_no" = "1"
+}
+
+resource "ncloud_server" "server" {
+  "server_image_product_code" = "SPSW0LINUX000046" #Conditional "server_image_product_code" OR "member_server_image_no"
+  "server_product_code"       = "SPSVRSTAND000004" #Optional
+
+  "tag_list" = [
+    {
+      "tag_key"   = "samplekey1"
+      "tag_value" = "samplevalue1"
+    },
+    {
+      "tag_key"   = "samplekey2"
+      "tag_value" = "samplevalue2"
+    },
+  ]
+}
