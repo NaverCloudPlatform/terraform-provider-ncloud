879ff707921fde99129e72bad3004db942e80516
sangkyu-kim <40969251+sangkyu-kim@users.noreply.github.com>
2020-07-29 16:49:00 +0900

    Update ncloud/resource_ncloud_vpc_test.go
    Co-authored-by: Yu SungDuk <sungduk.yu@navercorp.com>

--- ncloud/resource_ncloud_vpc_test.go
+++ ncloud/resource_ncloud_vpc_test.go
@@ -42,7 +42,7 @@ func TestAccResourceNcloudVpc_Update(t *testing.T) {
 	name := fmt.Sprintf("testacc-vpc-basic-%d", rInt)
 	resourceName := "ncloud_vpc.test"
 
-	resource.ParallelTest(t, resource.TestCase{
+	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
