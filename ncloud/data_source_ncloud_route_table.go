0fd21335175d8c19ed5fba778f362adab17a4db1
sangkyu.kim <sangkyu.kim@navercorp.com>
2020-08-13 13:39:12 +0900

    change `route_table` return type, refactor code

--- ncloud/data_source_ncloud_route_table.go
+++ ncloud/data_source_ncloud_route_table.go
@@ -5,59 +5,52 @@ import (
 )
 
 func dataSourceNcloudRouteTable() *schema.Resource {
-	return &schema.Resource{
-		Read: dataSourceNcloudRouteTableRead,
-
-		Schema: map[string]*schema.Schema{
-			"route_table_no": {
-				Type:     schema.TypeString,
-				Required: true,
-			},
-			"vpc_no": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-			"supported_subnet_type": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-			"name": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-			"is_default": {
-				Type:     schema.TypeBool,
-				Computed: true,
-			},
-			"description": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-			"status": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-		},
+	fieldMap := make(map[string]*schema.Schema)
+	fieldMap["route_table_no"] = &schema.Schema{
+		Type:     schema.TypeString,
+		Optional: true,
+		Computed: true,
 	}
+	fieldMap["vpc_no"] = &schema.Schema{
+		Type:     schema.TypeString,
+		Optional: true,
+		Computed: true,
+	}
+	fieldMap["name"] = &schema.Schema{
+		Type:     schema.TypeString,
+		Optional: true,
+		Computed: true,
+	}
+	fieldMap["supported_subnet_type"] = &schema.Schema{
+		Type:     schema.TypeString,
+		Optional: true,
+		Computed: true,
+	}
+	fieldMap["filter"] = dataSourceFiltersSchema()
+
+	return GetSingularDataSourceItemSchema(resourceNcloudRouteTable(), fieldMap, dataSourceNcloudRouteTableRead)
 }
 
 func dataSourceNcloudRouteTableRead(d *schema.ResourceData, meta interface{}) error {
 	config := meta.(*ProviderConfig)
 
-	instance, err := getRouteTableInstance(config, d.Get("route_table_no").(string))
+	resources, err := getRouteTableListFiltered(d, config)
 
 	if err != nil {
 		return err
 	}
 
-	d.SetId(*instance.RouteTableNo)
-	d.Set("route_table_no", instance.RouteTableNo)
-	d.Set("name", instance.RouteTableName)
-	d.Set("description", instance.RouteTableDescription)
-	d.Set("vpc_no", instance.VpcNo)
-	d.Set("supported_subnet_type", instance.SupportedSubnetType.Code)
-	d.Set("is_default", instance.IsDefault)
-	d.Set("status", instance.RouteTableStatus.Code)
+	if err := validateOneResult(len(resources)); err != nil {
+		return err
+	}
+
+	for k, v := range resources[0] {
+		if k == "id" {
+			d.SetId(v.(string))
+			continue
+		}
+		d.Set(k, v)
+	}
 
 	return nil
 }
