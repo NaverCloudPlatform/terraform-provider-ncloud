74b73f6f1bf759fa963d5f06316fad39d19b90bd
Yu SungDuk <sungduk.yu@navercorp.com>
2020-08-20 17:27:40 +0900

    region attribute is global, so move it to provider structure

--- ncloud/data_source_ncloud_zones_test.go
+++ ncloud/data_source_ncloud_zones_test.go
@@ -3,8 +3,6 @@ package ncloud
 import (
 	"testing"
 
-	"regexp"
-
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 )
 
@@ -25,50 +23,6 @@ func TestAccDataSourceNcloudZonesBasic(t *testing.T) {
 	})
 }
 
-func TestAccDataSourceNcloudZonesByRegionCode(t *testing.T) {
-	t.Parallel()
-
-	resource.Test(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheck(t) },
-		Providers: testAccProviders,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccDataSourceNcloudZonesByRegionCodeConfig,
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckDataSourceID("data.ncloud_zones.zones"),
-				),
-			},
-		},
-	})
-}
-
-func TestAccDataSourceNcloudZonesByInvalidRegionCode(t *testing.T) {
-	t.Parallel()
-
-	resource.Test(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheck(t) },
-		Providers: testAccProviders,
-		Steps: []resource.TestStep{
-			{
-				Config:      testAccDataSourceNcloudZonesByInvalidRegionCodeConfig,
-				ExpectError: regexp.MustCompile(`no region data for (.*) please change region_code and try again`),
-			},
-		},
-	})
-}
-
 var testAccDataSourceNcloudZonesConfig = `
 data "ncloud_zones" "zones" {}
-`
-
-var testAccDataSourceNcloudZonesByRegionCodeConfig = `
-data "ncloud_zones" "zones" {
-	region = "JPN"
-}
-`
-
-var testAccDataSourceNcloudZonesByInvalidRegionCodeConfig = `
-data "ncloud_zones" "zones" {
-	region = "INVALID"
-}
 `
