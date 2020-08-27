614ed9985831d8078dee2df4e74014d915e56ef3
sangkyu-kim <40969251+sangkyu-kim@users.noreply.github.com>
2020-08-27 15:56:20 +0900

    #31 init script (#65)
    * implement init script for VPC

---
+++ ncloud/data_source_ncloud_init_script_test.go
@@ -0,0 +1,73 @@
+package ncloud
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccDataSourceNcloudInitScriptBasic(t *testing.T) {
+	resourceName := "ncloud_init_script.foo"
+	dataName := "data.ncloud_init_script.by_id"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceNcloudInitScriptConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckDataSourceID(dataName),
+					resource.TestCheckResourceAttrPair(dataName, "name", resourceName, "name"),
+					resource.TestCheckResourceAttrPair(dataName, "description", resourceName, "description"),
+					resource.TestCheckResourceAttrPair(dataName, "os_type", resourceName, "os_type"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDataSourceNcloudInitScriptFilter(t *testing.T) {
+	resourceName := "ncloud_init_script.foo"
+	dataName := "data.ncloud_init_script.by_filter"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceNcloudInitScriptConfigFilter,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckDataSourceID("data.ncloud_init_script.by_filter"),
+					testAccCheckDataSourceID(dataName),
+					resource.TestCheckResourceAttrPair(dataName, "name", resourceName, "name"),
+					resource.TestCheckResourceAttrPair(dataName, "description", resourceName, "description"),
+					resource.TestCheckResourceAttrPair(dataName, "os_type", resourceName, "os_type"),
+				),
+			},
+		},
+	})
+}
+
+var testAccDataSourceNcloudInitScriptConfig = `
+resource "ncloud_init_script" "foo" {
+	content = "#!/usr/bin/env\nls -al"
+}
+
+data "ncloud_init_script" "by_id" {
+	init_script_no = ncloud_init_script.foo.id
+}
+`
+
+var testAccDataSourceNcloudInitScriptConfigFilter = `
+resource "ncloud_init_script" "foo" {
+	content = "#!/usr/bin/env\nls -al"
+}
+
+data "ncloud_init_script" "by_filter" {
+	filter {
+		name   = "init_script_no"
+		values = [ncloud_init_script.foo.id]
+	}
+}
+`
