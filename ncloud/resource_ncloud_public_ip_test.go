4ef22bcbb5cc07aaf5cfdd4de996f3a49ee868bd
sangkyu.kim <sangkyu.kim@navercorp.com>
2020-10-09 20:10:38 +0900

    Fix dependency limitation of creation public ip

--- ncloud/resource_ncloud_public_ip_test.go
+++ ncloud/resource_ncloud_public_ip_test.go
@@ -12,49 +12,59 @@ import (
 )
 
 func TestAccResourceNcloudPublicIpInstance_classic_basic(t *testing.T) {
-	testAccResourceNcloudPublicIpInstanceBasic(t, false)
-}
-
-func TestAccResourceNcloudPublicIpInstance_vpc_basic(t *testing.T) {
-	testAccResourceNcloudPublicIpInstanceBasic(t, true)
-}
-
-func testAccResourceNcloudPublicIpInstanceBasic(t *testing.T, isVpc bool) {
 	instance := map[string]interface{}{}
 
 	description := fmt.Sprintf("test-public-ip-basic-%s", acctest.RandString(5))
 	resourceName := "ncloud_public_ip.public_ip"
 
-	testCheckAttribute := func() func(*terraform.State) error {
-		return func(*terraform.State) error {
-			config := getTestProvider(isVpc).Meta().(*ProviderConfig)
-
-			if instance["status"] != GetValueClassicOrVPC(config, "CREAT", "RUN") {
-				return fmt.Errorf("invalid public ip status: %s ", instance["status"])
-			}
-
-			return nil
-		}
-	}
-
-	provider := getTestProvider(isVpc)
-
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
-		Providers: getTestAccProviders(isVpc),
+		Providers: testAccClassicProviders,
 		CheckDestroy: func(s *terraform.State) error {
-			return testAccCheckPublicIpInstanceDestroy(s, provider)
+			return testAccCheckPublicIpInstanceDestroy(s, testAccClassicProvider)
 		},
 		Steps: []resource.TestStep{
 			{
-				Config: testAccPublicIpInstanceConfig(description),
+				Config: testAccPublicIpInstanceClassicConfig(description),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckPublicIpInstanceExists(resourceName, instance, provider),
-					testCheckAttribute(),
+					testAccCheckPublicIpInstanceExists(resourceName, instance, testAccClassicProvider),
 					resource.TestMatchResourceAttr(resourceName, "id", regexp.MustCompile(`^\d+$`)),
 					resource.TestMatchResourceAttr(resourceName, "public_ip_no", regexp.MustCompile(`^\d+$`)),
 					resource.TestMatchResourceAttr(resourceName, "public_ip", regexp.MustCompile(`^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$`)),
 					resource.TestCheckResourceAttr(resourceName, "description", description),
+				),
+			},
+			{
+				ResourceName:            resourceName,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"server_instance_no"},
+			},
+		},
+	})
+}
+
+func TestAccResourceNcloudPublicIpInstance_vpc_basic(t *testing.T) {
+	instance := map[string]interface{}{}
+
+	name := fmt.Sprintf("test-public-ip-basic-%s", acctest.RandString(5))
+	resourceName := "ncloud_public_ip.public_ip"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		CheckDestroy: func(s *terraform.State) error {
+			return testAccCheckPublicIpInstanceDestroy(s, testAccProvider)
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: testAccPublicIpInstanceVpcConfig(name),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckPublicIpInstanceExists(resourceName, instance, testAccProvider),
+					resource.TestMatchResourceAttr(resourceName, "id", regexp.MustCompile(`^\d+$`)),
+					resource.TestMatchResourceAttr(resourceName, "public_ip_no", regexp.MustCompile(`^\d+$`)),
+					resource.TestMatchResourceAttr(resourceName, "public_ip", regexp.MustCompile(`^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$`)),
+					resource.TestCheckResourceAttr(resourceName, "description", name),
 				),
 			},
 			{
@@ -188,12 +198,58 @@ func testAccCheckPublicIpInstanceDestroy(s *terraform.State, provider *schema.Pr
 	return nil
 }
 
-func testAccPublicIpInstanceConfig(description string) string {
+func testAccPublicIpInstanceClassicConfig(description string) string {
 	return fmt.Sprintf(`
+resource "ncloud_login_key" "loginkey" {
+	key_name = "%[1]s-key"
+}
+
+resource "ncloud_server" "server" {
+	name = "%[1]s"
+	server_image_product_code = "SPSW0LINUX000032"
+	server_product_code = "SPSVRSTAND000004"
+	login_key_name = "${ncloud_login_key.loginkey.key_name}"
+}
+
 resource "ncloud_public_ip" "public_ip" {
-	description = "%s"
+	description = "%[1]s"
+	depends_on = [ncloud_server.server]
 }
 `, description)
+}
+
+func testAccPublicIpInstanceVpcConfig(name string) string {
+	return fmt.Sprintf(`
+resource "ncloud_login_key" "loginkey" {
+	key_name = "%[1]s-key"
+}
+
+resource "ncloud_vpc" "test" {
+	name               = "%[1]s"
+	ipv4_cidr_block    = "10.5.0.0/16"
+}
+
+resource "ncloud_subnet" "test" {
+	vpc_no             = ncloud_vpc.test.vpc_no
+	name               = "%[1]s"
+	subnet             = "10.5.0.0/24"
+	zone               = "KR-2"
+	network_acl_no     = ncloud_vpc.test.default_network_acl_no
+	subnet_type        = "PUBLIC"
+	usage_type         = "GEN"
+}
+
+resource "ncloud_server" "server" {
+	subnet_no = ncloud_subnet.test.id
+	name = "%[1]s"
+	server_image_product_code = "SW.VSVR.OS.LNX64.CNTOS.0703.B050"
+	login_key_name = ncloud_login_key.loginkey.key_name
+}
+
+resource "ncloud_public_ip" "public_ip" {
+	depends_on = [ncloud_server.server]
+	description = "%[1]s"
+}`, name)
 }
 
 func testAccPublicIpInstanceConfigClassicServer(serverNameFoo, serverNameBar, serverInstanceNo string) string {
@@ -219,6 +275,7 @@ resource "ncloud_server" "bar" {
 resource "ncloud_public_ip" "public_ip" {
 	description = "%[1]s"
 	server_instance_no = "%[3]s"
+	depends_on = [ncloud_server.foo]
 }
 `, serverNameFoo, serverNameBar, serverInstanceNo)
 }
@@ -263,6 +320,7 @@ resource "ncloud_server" "bar" {
 resource "ncloud_public_ip" "public_ip" {
 	description = "%[1]s"
 	server_instance_no = "%[3]s"
+	depends_on  = [ncloud_server.foo]
 }
 `, serverNameFoo, serverNameBar, serverInstanceNo)
 }
