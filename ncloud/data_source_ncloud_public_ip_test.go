3de7df1449ec90345b55fdc524a8851bf3448e35
sangkyu.kim <sangkyu.kim@navercorp.com>
2020-10-09 20:10:38 +0900

    Fix dependency limitation of creation public ip

--- ncloud/data_source_ncloud_public_ip_test.go
+++ ncloud/data_source_ncloud_public_ip_test.go
@@ -1,6 +1,8 @@
 package ncloud
 
 import (
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
 	"regexp"
 	"testing"
 
@@ -8,23 +10,16 @@ import (
 )
 
 func TestAccDataSourceNcloudPublicIp_classic_basic(t *testing.T) {
-	testAccDataSourceNcloudPublicIpBasic(t, false)
-}
-
-func TestAccDataSourceNcloudPublicIp_vpc_basic(t *testing.T) {
-	testAccDataSourceNcloudPublicIpBasic(t, true)
-}
-
-func testAccDataSourceNcloudPublicIpBasic(t *testing.T, isVpc bool) {
 	resourceName := "ncloud_public_ip.public_ip"
 	dataName := "data.ncloud_public_ip.test"
+	name := fmt.Sprintf("tf-public-ip-basic-%s", acctest.RandString(5))
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
-		Providers: getTestAccProviders(isVpc),
+		Providers: testAccClassicProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDataSourceNcloudPublicIpConfig,
+				Config: testAccDataSourceNcloudPublicIpClassicConfig(name),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckDataSourceID(dataName),
 					resource.TestCheckResourceAttrPair(dataName, "description", resourceName, "description"),
@@ -36,6 +31,29 @@ func testAccDataSourceNcloudPublicIpBasic(t *testing.T, isVpc bool) {
 					resource.TestCheckResourceAttrPair(dataName, "zone", resourceName, "zone"),
 					resource.TestCheckResourceAttrPair(dataName, "internet_line_type", resourceName, "internet_line_type"),
 					resource.TestCheckResourceAttrPair(dataName, "kind_type", resourceName, "kind_type"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDataSourceNcloudPublicIp_vpc_basic(t *testing.T) {
+	resourceName := "ncloud_public_ip.public_ip"
+	dataName := "data.ncloud_public_ip.test"
+	name := fmt.Sprintf("tf-public-ip-basic-%s", acctest.RandString(5))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceNcloudPublicIpVpcConfig(name),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckDataSourceID(dataName),
+					resource.TestCheckResourceAttrPair(dataName, "description", resourceName, "description"),
+					resource.TestCheckResourceAttrPair(dataName, "public_ip", resourceName, "public_ip"),
+					resource.TestCheckResourceAttrPair(dataName, "server_instance_no", resourceName, "server_instance_no"),
+					resource.TestCheckResourceAttrPair(dataName, "server_name", resourceName, "server_name"),
 				),
 			},
 		},
@@ -84,12 +102,67 @@ func TestAccDataSourceNcloudPublicIpSearch(t *testing.T) {
 	})
 }
 
-var testAccDataSourceNcloudPublicIpConfig = `
-resource "ncloud_public_ip" "public_ip" {}
+func testAccDataSourceNcloudPublicIpClassicConfig(name string) string {
+	return fmt.Sprintf(`
+resource "ncloud_login_key" "loginkey" {
+	key_name = "%[1]s-key"
+}
+
+resource "ncloud_server" "server" {
+	name = "%[1]s"
+	server_image_product_code = "SPSW0LINUX000032"
+	server_product_code = "SPSVRSTAND000004"
+	login_key_name = "${ncloud_login_key.loginkey.key_name}"
+}
+
+resource "ncloud_public_ip" "public_ip" {
+	description = "%[1]s"
+	depends_on = [ncloud_server.server]
+}
+
 data "ncloud_public_ip" "test" {
 	public_ip_no = ncloud_public_ip.public_ip.id
 }
-`
+`, name)
+}
+
+func testAccDataSourceNcloudPublicIpVpcConfig(name string) string {
+	return fmt.Sprintf(`
+resource "ncloud_login_key" "loginkey" {
+	key_name = "%[1]s-key"
+}
+
+resource "ncloud_vpc" "test" {
+	name               = "%[1]s"
+	ipv4_cidr_block    = "10.5.0.0/16"
+}
+
+resource "ncloud_subnet" "test" {
+	vpc_no             = ncloud_vpc.test.vpc_no
+	name               = "%[1]s"
+	subnet             = "10.5.0.0/24"
+	zone               = "KR-2"
+	network_acl_no     = ncloud_vpc.test.default_network_acl_no
+	subnet_type        = "PUBLIC"
+	usage_type         = "GEN"
+}
+
+resource "ncloud_server" "server" {
+	subnet_no = ncloud_subnet.test.id
+	name = "%[1]s"
+	server_image_product_code = "SW.VSVR.OS.LNX64.CNTOS.0703.B050"
+	login_key_name = ncloud_login_key.loginkey.key_name
+}
+
+resource "ncloud_public_ip" "public_ip" {
+	depends_on = [ncloud_server.server]
+}
+
+data "ncloud_public_ip" "test" {
+	public_ip_no = ncloud_public_ip.public_ip.id
+}
+`, name)
+}
 
 var testAccDataSourceNcloudPublicIpAssociatedConfig = `
 data "ncloud_public_ip" "test" {
