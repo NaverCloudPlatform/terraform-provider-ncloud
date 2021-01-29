7017b77e4f862cb291a4a0e7bde6e6ed324717bb
uiseon.yu <dmltjs851@gmail.com>
2021-01-29 15:45:08 +0900

    Implement resource auto_scaling_policy

--- ncloud/types.go
+++ ncloud/types.go
@@ -27,6 +27,16 @@ type AutoScalingGroup struct {
 	AccessControlGroupNoList []*string `json:"access_control_group_no_list,omitempty"`
 }
 
+type AutoScalingPolicy struct {
+	AutoScalingPolicyNo   *string `json:"auto_scaling_policy_no,omitempty"`
+	AutoScalingPolicyName *string `json:"name,omitempty"`
+	AutoScalingGroupNo    *string `json:"auto_scaling_group_no,omitempty"`
+	AdjustmentTypeCode    *string `json:"adjustment_type_code,omitempty"`
+	ScalingAdjustment     *int32  `json:"scaling_adjustment,omitempty"`
+	Cooldown              *int32  `json:"cooldown,omitempty"`
+	MinAdjustmentStep     *int32  `json:"min_adjustment_step,omitempty"`
+}
+
 type InAutoScalingGroupServerInstance struct {
 	HealthStatus        *string `json:"health_status,omitempty"`
 	LifecycleState      *string `json:"lifecycle_state,omitempty"`
