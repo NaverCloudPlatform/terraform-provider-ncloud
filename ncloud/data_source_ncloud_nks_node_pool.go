22b9f187eb22315caa9f17a8959e6206d8ccfeb6
MinGyum Kim <mingyum.kim@gmail.com>
2023-03-24 01:31:20 +0900

    fix: nks_node_pool.subnet_no index out of range error (#264)

--- ncloud/data_source_ncloud_nks_node_pool.go
+++ ncloud/data_source_ncloud_nks_node_pool.go
@@ -137,7 +137,9 @@ func dataSourceNcloudNKSNodePoolRead(ctx context.Context, d *schema.ResourceData
 	d.Set("product_code", nodePool.ProductCode)
 	d.Set("node_count", nodePool.NodeCount)
 	d.Set("k8s_version", nodePool.K8sVersion)
-	d.Set("subnet_no", strconv.Itoa(int(ncloud.Int32Value(nodePool.SubnetNoList[0]))))
+	if len(nodePool.SubnetNoList) > 0 {
+		d.Set("subnet_no", strconv.Itoa(int(ncloud.Int32Value(nodePool.SubnetNoList[0]))))
+	}
 
 	if err := d.Set("autoscale", flattenNKSNodePoolAutoScale(nodePool.Autoscale)); err != nil {
 		log.Printf("[WARN] Error setting Autoscale set for (%s): %s", d.Id(), err)
