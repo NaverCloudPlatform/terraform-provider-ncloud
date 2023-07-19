3f5435090ce1648e3029ab0ea05cf7e3583dc88c
Younghwan Kim <88774925+royroyee@users.noreply.github.com>
2023-07-19 19:30:33 +0900

    test: Update testAccResourceNcloudNatGatewayConfigOnlyRequiredParam t… (#303)
    * test: Update testAccResourceNcloudNatGatewayConfigOnlyRequiredParam to include subnet_no
    
    * test: Update testAccResourceNcloudNatGatewayConfigOnlyRequiredParam to include subnet_no
    
    ---------
    
    Co-authored-by: Younghwan Kim <yh.kim@alpacanetworks.com>

--- internal/service/vpc/nat_gateway_test.go
+++ internal/service/vpc/nat_gateway_test.go
@@ -93,6 +93,7 @@ func TestAccResourceNcloudNatGateway_onlyRequiredParam(t *testing.T) {
 					resource.TestMatchResourceAttr(resourceName, "vpc_no", regexp.MustCompile(`^\d+$`)),
 					resource.TestMatchResourceAttr(resourceName, "nat_gateway_no", regexp.MustCompile(`^\d+$`)),
 					resource.TestMatchResourceAttr(resourceName, "name", regexp.MustCompile(`^[a-z0-9]+$`)),
+					resource.TestMatchResourceAttr(resourceName, "subnet_no", regexp.MustCompile(`^\d+$`)),
 				),
 			},
 			{
@@ -227,8 +228,18 @@ resource "ncloud_vpc" "vpc" {
 	ipv4_cidr_block = "10.3.0.0/16"
 }
 
+resource "ncloud_subnet" "subnet_public" {
+  vpc_no         = ncloud_vpc.vpc.id
+  subnet         = cidrsubnet(ncloud_vpc.vpc.ipv4_cidr_block, 8, 1)
+  zone           = "KR-1"
+  network_acl_no = ncloud_vpc.vpc.default_network_acl_no
+  subnet_type    = "PUBLIC"
+  usage_type     = "NATGW"
+}
+	
 resource "ncloud_nat_gateway" "nat_gateway" {
   vpc_no      = ncloud_vpc.vpc.vpc_no
+  subnet_no   = ncloud_subnet.subnet_public.id
   zone        = "KR-1"
 }
 `, name)
