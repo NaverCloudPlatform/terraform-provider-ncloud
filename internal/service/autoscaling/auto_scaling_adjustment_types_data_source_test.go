20d9f3cf731249df823499227c1af10e900ef176
royroyee <kim131325@naver.com>
2023-07-29 17:16:22 +0900

    Add: Test Cases for Ncloud Auto Scaling Adjustment Types Data Source

---
+++ internal/service/autoscaling/auto_scaling_adjustment_types_data_source_test.go
@@ -0,0 +1,146 @@
+package autoscaling_test
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	. "github.com/terraform-providers/terraform-provider-ncloud/internal/acctest"
+	"testing"
+)
+
+func TestAccDataSourceNcloudAutoScalingAdjustmentTypes_classic_basic(t *testing.T) {
+	dataName := "data.ncloud_auto_scaling_adjustment_types.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:  func() { TestAccPreCheck(t) },
+		Providers: GetTestAccProviders(false),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceNcloudAutoScalingAdjustmentTypesClassicConfig(),
+				Check: resource.ComposeTestCheckFunc(
+					TestAccCheckDataSourceID(dataName),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDataSourceNcloudAutoScalingAdjustmentTypes_vpc_basic(t *testing.T) {
+	dataName := "data.ncloud_auto_scaling_adjustment_types.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:  func() { TestAccPreCheck(t) },
+		Providers: GetTestAccProviders(false),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceNcloudAutoScalingAdjustmentTypesVpcConfig(),
+				Check: resource.ComposeTestCheckFunc(
+					TestAccCheckDataSourceID(dataName),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDataSourceNcloudAutoScalingAdjustmentTypes_classic_byFilterCode(t *testing.T) {
+	dataName := "data.ncloud_auto_scaling_adjustment_types.by_filter"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:  func() { TestAccPreCheck(t) },
+		Providers: GetTestAccProviders(false),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceNcloudAutoScalingAdjustmentTypesByFilterCodeConfig("EXACT"),
+				Check: resource.ComposeTestCheckFunc(
+					TestAccCheckDataSourceID(dataName),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDataSourceNcloudAutoScalingAdjustmentTypes_vpc_byFilterCode(t *testing.T) {
+	dataName := "data.ncloud_auto_scaling_adjustment_types.by_filter"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:  func() { TestAccPreCheck(t) },
+		Providers: GetTestAccProviders(false),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceNcloudAutoScalingAdjustmentTypesByFilterCodeConfig("EXACT"),
+				Check: resource.ComposeTestCheckFunc(
+					TestAccCheckDataSourceID(dataName),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceNcloudAutoScalingAdjustmentTypesClassicConfig() string {
+	return fmt.Sprint(`
+	resource "ncloud_launch_configuration" "lc" {
+		name = "my-lc"
+  		server_image_product_code = "SPSW0LINUX000046"
+  		server_product_code = "SPSVRSSD00000003"
+	}
+
+	resource "ncloud_auto_scaling_group" "asg" {
+  		launch_configuration_no = ncloud_launch_configuration.lc.launch_configuration_no
+  		min_size = 1
+		max_size = 1
+  		zone_no_list = ["2"]
+  		wait_for_capacity_timeout = "0"
+	}
+
+	resource "ncloud_auto_scaling_policy" "policy" {
+  		name = "my-policy"
+  		adjustment_type_code = data.ncloud_auto_scaling_adjustment_types.test.types[0].code
+  		scaling_adjustment = 2
+  		auto_scaling_group_no = ncloud_auto_scaling_group.asg.auto_scaling_group_no
+	}
+	
+	
+	data "ncloud_auto_scaling_adjustment_types" "test" {
+	
+	}
+	`)
+}
+
+func testAccDataSourceNcloudAutoScalingAdjustmentTypesVpcConfig() string {
+	return fmt.Sprint(`
+	resource "ncloud_launch_configuration" "lc" {
+		name = "my-lc"
+  		server_image_product_code = "SPSW0LINUX000046"
+  		server_product_code = "SPSVRSSD00000003"
+	}
+
+	resource "ncloud_auto_scaling_group" "asg" {
+  		launch_configuration_no = ncloud_launch_configuration.lc.launch_configuration_no
+  		min_size = 1
+		max_size = 1
+  		zone_no_list = ["2"]
+  		wait_for_capacity_timeout = "0"
+	}
+
+	resource "ncloud_auto_scaling_policy" "policy" {
+  		name = "my-policy"
+  		adjustment_type_code = data.ncloud_auto_scaling_adjustment_types.test.types[2].code 
+  		scaling_adjustment = 2
+  		auto_scaling_group_no = ncloud_auto_scaling_group.asg.auto_scaling_group_no
+	}
+	
+	
+	data "ncloud_auto_scaling_adjustment_types" "test" {
+	}
+	`)
+}
+
+func testAccDataSourceNcloudAutoScalingAdjustmentTypesByFilterCodeConfig(code string) string {
+	return fmt.Sprintf(`
+	data "ncloud_auto_scaling_adjustment_types" "by_filter" {
+  		filter {
+    		name   = "code"
+   			 values = ["%s"]
+	    }
+	}
+`, code)
+}
