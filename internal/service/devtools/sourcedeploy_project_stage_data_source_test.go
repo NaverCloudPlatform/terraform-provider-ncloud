c28445894e696c0ecb901bf163387d2160a8c841
soo <soo@zamface.kr>
2023-08-09 20:59:10 +0900

    fix : make sourcedeploy stage data source test to run

--- internal/service/devtools/sourcedeploy_project_stage_data_source_test.go
+++ internal/service/devtools/sourcedeploy_project_stage_data_source_test.go
@@ -9,14 +9,15 @@ import (
 	. "github.com/terraform-providers/terraform-provider-ncloud/internal/acctest"
 )
 
-func TestAccDataSourceNcloudSourceDeployStage(t *testing.T) {
+func TestAccDataSourceNcloudSourceDeploySingleStage(t *testing.T) {
 	stageNameSvr := getTestSourceDeployStageName() + "svr"
+	productCode := "SVR.VSVR.STAND.C002.M008.NET.HDD.B050.G002"
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:                 func() { TestAccPreCheck(t) },
 		ProtoV5ProviderFactories: ProtoV5ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDataSourceNcloudSourceDeployStageConfig(stageNameSvr),
+				Config: testAccDataSourceNcloudSourceDeployStageConfig(stageNameSvr, productCode),
 				Check: resource.ComposeTestCheckFunc(
 					TestAccCheckDataSourceID("data.ncloud_sourcedeploy_project_stage.stage"),
 				),
@@ -25,15 +26,33 @@ func TestAccDataSourceNcloudSourceDeployStage(t *testing.T) {
 	})
 }
 
-func testAccDataSourceNcloudSourceDeployStageConfig(stageNameSvr string) string {
+func testAccDataSourceNcloudSourceDeployStageConfig(stageNameSvr string, productCode string) string {
 	return fmt.Sprintf(`
-data "ncloud_server" "server" {
-	filter {
-		name   = "name"
-		values = ["%[1]s"]
-	}
+resource "ncloud_login_key" "loginkey" {
+	key_name = "%[2]s-key"
 }
 
+resource "ncloud_vpc" "vpc" {
+	ipv4_cidr_block    = "10.5.0.0/16"
+}
+
+resource "ncloud_subnet" "subnet" {
+	vpc_no             = ncloud_vpc.vpc.vpc_no
+	name               = "%[1]s"
+	subnet             = "10.5.0.0/24"
+	zone               = "KR-2"
+	network_acl_no     = ncloud_vpc.vpc.default_network_acl_no
+	subnet_type        = "PUBLIC"
+	usage_type         = "GEN"
+}
+
+resource "ncloud_server" "server" {
+	subnet_no = ncloud_subnet.subnet.id
+	name = "%[1]s"
+	server_image_product_code = "SW.VSVR.OS.LNX64.CNTOS.0703.B050"
+	server_product_code = "%[3]s"
+	login_key_name = ncloud_login_key.loginkey.key_name
+}
 resource "ncloud_sourcedeploy_project" "sd_project" {
 	name = "tf-test-project2"
 }
@@ -44,7 +63,7 @@ resource "ncloud_sourcedeploy_project_stage" "svr_stage" {
 	target_type = "Server"
 	config {
 		server{
-			id = data.ncloud_server.server.id
+			id = ncloud_server.server.id
 		} 
 	}
 }
@@ -53,5 +72,5 @@ data "ncloud_sourcedeploy_project_stage" "stage"{
 	project_id = ncloud_sourcedeploy_project.sd_project.id
 	id         = ncloud_sourcedeploy_project_stage.svr_stage.id
 }
-`, TF_TEST_SD_SERVER_NAME, stageNameSvr)
+`, TF_TEST_SD_SERVER_NAME, stageNameSvr, productCode)
 }
