f27d470c8d46d86a7b40fdb26f2d59f779c723e1
JungChan Son <rnrmfjc@gmail.com>
2023-08-12 20:57:11 +0900

    feature: add schema

--- internal/service/cloudmysql/cloud_mysql_data_source.go
+++ internal/service/cloudmysql/cloud_mysql_data_source.go
@@ -6,7 +6,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	. "github.com/terraform-providers/terraform-provider-ncloud/internal/common"
 	"github.com/terraform-providers/terraform-provider-ncloud/internal/conn"
-	"github.com/terraform-providers/terraform-provider-ncloud/internal/verify"
 )
 
 func DataSourceNcloudMysql() *schema.Resource {
@@ -15,6 +14,120 @@ func DataSourceNcloudMysql() *schema.Resource {
 			Type:     schema.TypeString,
 			Optional: true,
 			Computed: true,
+		},
+		"service_name": {
+			Type:     schema.TypeString,
+			Optional: true,
+			Computed: true,
+		},
+		"engine_version": {
+			Type:     schema.TypeString,
+			Optional: true,
+			Computed: true,
+		},
+		"is_ha": {
+			Type:        schema.TypeBool,
+			Optional:    true,
+			Computed:    true,
+			Description: "default: true",
+		},
+		"is_multi_zone": {
+			Type:        schema.TypeBool,
+			Optional:    true,
+			Computed:    true,
+			Description: "default: false",
+		},
+		"is_backup": {
+			Type:        schema.TypeBool,
+			Optional:    true,
+			Computed:    true,
+			Description: "default: true",
+		},
+		"backup_file_retention_period": {
+			Type:        schema.TypeInt,
+			Optional:    true,
+			Computed:    true,
+			Description: "default: 1(1 day)",
+		},
+		"backup_time": {
+			Type:        schema.TypeString,
+			Optional:    true,
+			Computed:    true,
+			Description: "ex) 01:15",
+		},
+		"port": {
+			Type:        schema.TypeInt,
+			Optional:    true,
+			Computed:    true,
+			Description: "default: 3306",
+		},
+		"image_product_code": {
+			Type:     schema.TypeString,
+			Optional: true,
+			Computed: true,
+		},
+		"instance_no": {
+			Type:     schema.TypeString,
+			Optional: true,
+			Computed: true,
+		},
+		"cloud_mysql_server_instance_list": {
+			Type: schema.TypeList,
+			Optional: true,
+			Computed: true,
+			Elem: &schema.Resource{
+				Schema: map[string]*schema.Schema{
+					"cloud_mysql_server_instance_no": {
+						Type: schema.TypeString,
+						Computed: true,
+						ForceNew: true,
+
+					},
+					"cloud_mysql_server_name": {
+						Type: schema.TypeString,
+						Computed: true,
+						ForceNew: true,
+
+					},
+					"region_code": {
+						Type: schema.TypeString,
+						Computed: true,
+						ForceNew: true,
+
+					},
+					"zone_code": {
+						Type: schema.TypeString,
+						Computed: true,
+						ForceNew: true,
+
+					},
+					"vpc_no": {
+						Type: schema.TypeString,
+						Computed: true,
+						ForceNew: true,
+					},
+					"subnet_no": {
+						Type: schema.TypeString,
+						Computed: true,
+						ForceNew: true,
+					},
+					"data_storage_size": {
+						Type: schema.TypeInt,
+						Computed: true,
+						ForceNew: true,
+					},
+					"used_data_storage_size": {
+						Type: schema.TypeInt,
+						Computed: true,
+						ForceNew: true,
+					},
+					"cpu_count": {
+						Type: schema.TypeInt,
+						Computed: true,
+						ForceNew: true,
+					},
+				},
+			},
 		},
 		"filter": DataSourceFiltersSchema(),
 	}
@@ -29,19 +142,73 @@ func dataSourceNcloudMysqlRead(d *schema.ResourceData, meta interface{}) error {
 		return NotSupportClassic("data source `ncloud_mysql`")
 	}
 
-	r, err := getMysqlList(d, config)
+	instance, err := getMysqlList(d, config)
+	if err != nil {
+		return err
+	}
+	d.SetId(instance[0]["id"].(string))
 
+	detailInstance, err := getMysqlDetail(config, instance)
 	if err != nil {
 		return err
 	}
 
-	if err := verify.ValidateOneResult(len(r)); err != nil {
-		return err
+
+	resources := ConvertToArrayMap(detailInstance)
+	SetSingularResourceDataFromMap(d, resources[0])
+	return nil
+}
+
+func getMysqlDetail(config *conn.ProviderConfig, r []map[string]interface{}) ([]map[string]interface{}, error){
+	reqParams := &vmysql.GetCloudMysqlInstanceDetailRequest{
+		RegionCode:           &config.RegionCode,
+		CloudMysqlInstanceNo: ncloud.String(r[0]["id"].(string)),
 	}
 
-	SetSingularResourceDataFromMap(d, r[0])
+	resp, err := config.Client.Vmysql.V2Api.GetCloudMysqlInstanceDetail(reqParams)
+	if err != nil {
+		LogErrorResponse("getCloudMysqlDetail", err, reqParams)
+		return nil, err
+	}
+	LogResponse("GetMysqlDetail", resp)
 
-	return nil
+	var resources []map[string]interface{}
+	for _, r := range resp.CloudMysqlInstanceList {
+		instance := map[string]interface{}{
+			"id":                               *r.CloudMysqlInstanceNo,
+			"instance_no":                         *r.CloudMysqlInstanceNo,
+			"service_name":                     *r.CloudMysqlServiceName,
+			"engine_version":                   *r.EngineVersion,
+			"is_ha":                            *r.IsHa,
+			"is_multi_zone":                    *r.IsMultiZone,
+			"is_backup":                        *r.IsBackup,
+			"backup_file_retention_period":     *r.BackupFileRetentionPeriod,
+			"backup_time":                      *r.BackupTime,
+			"port":                             *r.CloudMysqlPort,
+			"image_product_code":               *r.CloudMysqlImageProductCode,
+			"cloud_mysql_server_instance_list": map[string]interface{}{},
+		}
+		var serverInstanceList []map[string]interface{}
+		for _, v := range r.CloudMysqlServerInstanceList {
+			ins := map[string]interface{}{
+				"cloud_mysql_server_instance_no": v.CloudMysqlServerInstanceNo,
+				"cloud_mysql_server_name":        v.CloudMysqlServerName,
+				"region_code":                    v.RegionCode,
+				"zone_code":                      v.ZoneCode,
+				"vpc_no":                         v.VpcNo,
+				"subnet_no":                      v.SubnetNo,
+				"data_storage_size":              v.DataStorageSize,
+				"used_data_storage_size":         v.UsedDataStorageSize,
+				"cpu_count":                      v.CpuCount,
+			}
+
+			serverInstanceList = append(serverInstanceList, ins)
+		}
+		instance["cloud_mysql_server_instance_list"] = serverInstanceList
+		resources = append(resources, instance)
+	}
+
+	return resources, nil
 }
 
 func getMysqlList(d *schema.ResourceData, config *conn.ProviderConfig) ([]map[string]interface{}, error) {
@@ -54,6 +221,18 @@ func getMysqlList(d *schema.ResourceData, config *conn.ProviderConfig) ([]map[st
 	if v, ok := d.GetOk("id"); ok {
 		reqParams.CloudMysqlInstanceNoList = []*string{ncloud.String(v.(string))}
 	}
+	if v, ok := d.GetOk("zone_code"); ok {
+		reqParams.ZoneCode = ncloud.String(v.(string))
+	}
+	if v, ok := d.GetOk("vpc_no"); ok {
+		reqParams.VpcNo = ncloud.String(v.(string))
+	}
+	if v, ok := d.GetOk("subnet_no"); ok {
+		reqParams.SubnetNo = ncloud.String(v.(string))
+	}
+	if v, ok := d.GetOk("service_name"); ok {
+		reqParams.CloudMysqlServiceName = ncloud.String(v.(string))
+	}
 
 	LogCommonRequest("getCloudMysqlList", reqParams)
 
@@ -64,12 +243,22 @@ func getMysqlList(d *schema.ResourceData, config *conn.ProviderConfig) ([]map[st
 	}
 	LogResponse("getCloudMysqlList", resp)
 
-	var resourcesList []map[string]interface{}
+	var resources []map[string]interface{}
 	for _, r := range resp.CloudMysqlInstanceList {
+		instance := map[string]interface{}{
+			"id":                           *r.CloudMysqlInstanceNo,
+			"instance_no":                     *r.CloudMysqlInstanceNo,
+			"service_name":                 *r.CloudMysqlServiceName,
+			"is_ha":                        *r.IsHa,
+			"is_multi_zone":                *r.IsMultiZone,
+		}
 
-		instance := ConvertToMap(r)
-		resourcesList = append(resourcesList, instance)
+		resources = append(resources, instance)
 	}
 
-	return resourcesList, nil
+	if f, ok := d.GetOk("filter"); ok {
+		resources = ApplyFilters(f.(*schema.Set), resources, DataSourceNcloudMysql().Schema)
+	}
+
+	return resources, nil
 }
