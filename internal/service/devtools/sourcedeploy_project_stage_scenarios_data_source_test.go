5a37f9c27d258d30c9443f99c6859252026d7d6f
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2023-08-16 15:16:35 +0900

    fix: devtoolsSource(Deploy|Commit)* data sources test failure (#343)
    * fix : make sourcedeploy stage data source test to run
    
    * fix : make sourcedeploy scenarios project data source test to run
    
    ---------
    
    Co-authored-by: soo <soo@zamface.kr>

--- internal/service/devtools/sourcedeploy_project_stage_scenarios_data_source_test.go
+++ internal/service/devtools/sourcedeploy_project_stage_scenarios_data_source_test.go
@@ -10,12 +10,14 @@ import (
 )
 
 func TestAccDataSourceNcloudSourceDeployScenarios(t *testing.T) {
+	stageNameSvr := GetTestSourceDeployScenarioName() + "svr"
+	productCode := "SVR.VSVR.STAND.C002.M008.NET.HDD.B050.G002"
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:                 func() { TestAccPreCheck(t) },
 		ProtoV5ProviderFactories: ProtoV5ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDataSourceNcloudSourceDeployScenariosConfig(),
+				Config: testAccDataSourceNcloudSourceDeployScenariosConfig(stageNameSvr, productCode),
 				Check: resource.ComposeTestCheckFunc(
 					TestAccCheckDataSourceID("data.ncloud_sourcedeploy_project_stage_scenarios.scenarios"),
 				),
@@ -24,26 +26,45 @@ func TestAccDataSourceNcloudSourceDeployScenarios(t *testing.T) {
 	})
 }
 
-func testAccDataSourceNcloudSourceDeployScenariosConfig() string {
+func testAccDataSourceNcloudSourceDeployScenariosConfig(stageNameSvr string, productCode string) string {
 	return fmt.Sprintf(`
-data "ncloud_server" "server" {
-	filter {
-		name   = "name"
-		values = ["%[1]s"]
-	}
+resource "ncloud_login_key" "loginkey" {
+	key_name = "%[2]s-key"
+}
+
+resource "ncloud_vpc" "vpc" {
+	ipv4_cidr_block    = "10.0.0.0/16"
+}
+
+resource "ncloud_subnet" "subnet" {
+	vpc_no             = ncloud_vpc.vpc.vpc_no
+	subnet             = "10.0.0.0/24"
+	zone               = "KR-2"
+	network_acl_no     = ncloud_vpc.vpc.default_network_acl_no
+	subnet_type        = "PUBLIC"
+	usage_type         = "GEN"
+}
+
+
+resource "ncloud_server" "server" {
+	subnet_no = ncloud_subnet.subnet.id
+	name = "%[2]s"
+	server_image_product_code = "SW.VSVR.OS.LNX64.CNTOS.0703.B050"
+	server_product_code = "%[3]s"
+	login_key_name = ncloud_login_key.loginkey.key_name
 }
 
 resource "ncloud_sourcedeploy_project" "sd_project" {
-	name = "tf-test-project"
+	name = "%[2]s"
 }
 
 resource "ncloud_sourcedeploy_project_stage" "svr_stage" {
 	project_id  = ncloud_sourcedeploy_project.sd_project.id
-	name		= "svr"
+	name		= "%[2]s"
 	target_type = "Server"
 	config {
 		server {
-			id = data.ncloud_server.server.id
+			id = ncloud_server.server.id
 		}
 	}
 }
@@ -52,5 +73,5 @@ data "ncloud_sourcedeploy_project_stage_scenarios" "scenarios"{
 	project_id = ncloud_sourcedeploy_project.sd_project.id
 	stage_id   = ncloud_sourcedeploy_project_stage.svr_stage.id
 }
-`, TF_TEST_SD_SERVER_NAME)
+`, TF_TEST_SD_SERVER_NAME, stageNameSvr, productCode)
 }
