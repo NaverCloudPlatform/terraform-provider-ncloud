ce3f5cd94497199ca9bac1b6a57453a0b3b33d4c
JungChan Son <rnrmfjc@gmail.com>
2023-08-18 18:18:26 +0900

    fix(auto_scaling_group.go): add 'server_product_code' to prevent error response

--- internal/service/autoscaling/auto_scaling_group_test.go
+++ internal/service/autoscaling/auto_scaling_group_test.go
@@ -231,7 +231,8 @@ func testAccCheckAutoScalingGroupExists(n string, a *autoscaling.AutoScalingGrou
 func testAccAutoScalingGroupClassicConfig() string {
 	return `
 resource "ncloud_launch_configuration" "lc" {
-	server_image_product_code = "SPSW0LINUX000046"
+    server_image_product_code = "SPSW0LINUX000046"
+    server_product_code = "SPSVRSSD00000003" // 추가
 }
 
 resource "ncloud_auto_scaling_group" "auto" {
