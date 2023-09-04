356481aec86c18e0dda9e92c39e7b020166d0eac
JungChan Son <rnrmfjc@gmail.com>
2023-09-04 19:20:33 +0900

    refactor(provider.go): change framwork resource

--- internal/provider/fwprovider/provider.go
+++ internal/provider/fwprovider/provider.go
@@ -70,6 +70,7 @@ func (p *fwprovider) DataSources(ctx context.Context) []func() datasource.DataSo
 	dataSources = append(dataSources, vpc.NewSubnetDataSource)
 	dataSources = append(dataSources, vpc.NewSubnetsDataSource)
 	dataSources = append(dataSources, vpc.NewNatGatewayDataSource)
+	dataSources = append(dataSources, vpc.NewVpcPeeringDataSource)
 
 	if err := errs.ErrorOrNil(); err != nil {
 		tflog.Warn(ctx, "registering resources", map[string]interface{}{
@@ -87,6 +88,7 @@ func (p *fwprovider) Resources(ctx context.Context) []func() resource.Resource {
 	resources = append(resources, vpc.NewVpcResource)
 	resources = append(resources, vpc.NewSubnetResource)
 	resources = append(resources, vpc.NewNatGatewayResource)
+	resources = append(resources, vpc.NewVpcPeeringResource)
 
 	if err := errs.ErrorOrNil(); err != nil {
 		tflog.Warn(ctx, "registering resources", map[string]interface{}{
