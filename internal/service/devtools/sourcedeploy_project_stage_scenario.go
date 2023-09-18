f49c12e7d37615b0c3f186ae9ccf813a1d08bfaa
Wonchul Lee <wch.lee@navercorp.com>
2023-09-18 17:39:20 +0900

    chore: Apply go fmt

--- internal/service/devtools/sourcedeploy_project_stage_scenario.go
+++ internal/service/devtools/sourcedeploy_project_stage_scenario.go
@@ -696,7 +696,7 @@ func getManifest(deployTargetType string, d *schema.ResourceData) (*vsourcedeplo
 		}
 
 		if param, ok := d.GetOk("config.0.manifest.0.path"); ok {
-			err := ValidateEmptyStringElement(param.([]interface{}));
+			err := ValidateEmptyStringElement(param.([]interface{}))
 			if err != nil {
 				return nil, fmt.Errorf("config.manifest.path cannot contain an empty string element")
 			}
