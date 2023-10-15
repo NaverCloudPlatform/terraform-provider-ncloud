2f07e23b0d4c673adc07509793a6d87218bcd14e
JungChan Son <rnrmfjc@gmail.com>
2023-10-15 21:44:10 +0900

    feature(mysql_test.go): change 'V5Provider' to 'V6Provider' and add expect cases of error

--- internal/service/mysql/mysql_test.go
+++ internal/service/mysql/mysql_test.go
@@ -56,7 +56,7 @@ func TestAccResourceNcloudMysql_vpc_isHa(t *testing.T) {
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { TestAccPreCheck(t) },
-		ProtoV5ProviderFactories: ProtoV5ProviderFactories,
+		ProtoV6ProviderFactories: ProtoV6ProviderFactories,
 		CheckDestroy:             testAccCheckMysqlDestroy,
 		Steps: []resource.TestStep{
 			{
@@ -81,7 +81,7 @@ func TestAccResourceNcloudMysql_vpc_isHa_options(t *testing.T) {
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { TestAccPreCheck(t) },
-		ProtoV5ProviderFactories: ProtoV5ProviderFactories,
+		ProtoV6ProviderFactories: ProtoV6ProviderFactories,
 		CheckDestroy:             testAccCheckMysqlDestroy,
 		Steps: []resource.TestStep{
 			{
@@ -93,7 +93,6 @@ func TestAccResourceNcloudMysql_vpc_isHa_options(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "is_multi_zone", "true"),
 					resource.TestCheckResourceAttr(resourceName, "is_storage_encryption", "true"),
 					resource.TestCheckResourceAttr(resourceName, "is_backup", "true"),
-					resource.TestCheckResourceAttr(resourceName, "is_automatic_backup", "true"),
 				),
 			},
 		},
@@ -107,7 +106,7 @@ func TestAccResourceNcloudMysql_vpc_auto_backup(t *testing.T) {
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { TestAccPreCheck(t) },
-		ProtoV5ProviderFactories: ProtoV5ProviderFactories,
+		ProtoV6ProviderFactories: ProtoV6ProviderFactories,
 		CheckDestroy:             testAccCheckMysqlDestroy,
 		Steps: []resource.TestStep{
 			{
@@ -118,7 +117,6 @@ func TestAccResourceNcloudMysql_vpc_auto_backup(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "is_ha", "false"),
 					resource.TestCheckResourceAttr(resourceName, "is_backup", "true"),
 					resource.TestCheckResourceAttr(resourceName, "backup_file_retention_period", "3"),
-					resource.TestCheckResourceAttr(resourceName, "is_automatic_backup", "true"),
 				),
 			},
 		},
@@ -132,7 +130,7 @@ func TestAccResourceNcloudMysql_vpc_not_auto_backup(t *testing.T) {
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { TestAccPreCheck(t) },
-		ProtoV5ProviderFactories: ProtoV5ProviderFactories,
+		ProtoV6ProviderFactories: ProtoV6ProviderFactories,
 		CheckDestroy:             testAccCheckMysqlDestroy,
 		Steps: []resource.TestStep{
 			{
@@ -143,9 +141,44 @@ func TestAccResourceNcloudMysql_vpc_not_auto_backup(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "is_ha", "false"),
 					resource.TestCheckResourceAttr(resourceName, "is_backup", "true"),
 					resource.TestCheckResourceAttr(resourceName, "backup_file_retention_period", "3"),
-					resource.TestCheckResourceAttr(resourceName, "is_automatic_backup", "false"),
 					resource.TestCheckResourceAttr(resourceName, "backup_time", "11:15"),
 				),
+			},
+		},
+	})
+}
+
+func TestAccResourceNcloudMysql_error_case(t *testing.T) {
+	testMysqlName := fmt.Sprintf("tf-mysql-%s", acctest.RandString(5))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { TestAccPreCheck(t) },
+		ProtoV6ProviderFactories: ProtoV6ProviderFactories,
+		CheckDestroy:             testAccCheckMysqlDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config:      testAccMysqlVpcConfigErrorCaseWhenIsHaSetFalse1(testMysqlName),
+				ExpectError: regexp.MustCompile("when `is_ha` is false, `is_multi_zone` parameter is not used"),
+			},
+			{
+				Config:      testAccMysqlVpcConfigErrorCaseWhenIsHaSetFalse2(testMysqlName),
+				ExpectError: regexp.MustCompile("when `is_ha` is false, `standby_master_subnet_no` is not used"),
+			},
+			{
+				Config:      testAccMysqlVpcConfigErrorCaseWhenIsHaSetFalse3(testMysqlName),
+				ExpectError: regexp.MustCompile("when `is_ha` is false, can't set true for `is_storage_encryption`"),
+			},
+			{
+				Config:      testAccMysqlVpcConfigErrorCaseWhenIsHaSetTrue(testMysqlName),
+				ExpectError: regexp.MustCompile("when `is_ha` is true, `is_backup` must be true or not be inputted"),
+			},
+			{
+				Config:      testAccMysqlVpcConfigErrorCaseWhenAutomaticBackup(testMysqlName),
+				ExpectError: regexp.MustCompile("when `is_backup` is true and `is_automactic_backup` is true, `backup_time`"),
+			},
+			{
+				Config:      testAccMysqlVpcConfigErrorCaseWhenEmptyBackupTime(testMysqlName),
+				ExpectError: regexp.MustCompile("when `is_backup` is true and `is_automactic_backup` is false, `backup_time`"),
 			},
 		},
 	})
@@ -224,7 +257,7 @@ resource "ncloud_mysql" "mysql" {
 	user_password = "t123456789!a"
 	host_ip = "192.168.0.1"
 	database_name = "test_db"
-	}
+}
 `, testMysqlName)
 }
 
@@ -369,4 +402,119 @@ resource "ncloud_mysql" "mysql" {
 	backup_time = "%[6]s"
 }
 `, name, isHa, isBackup, isAutomaticBackup, backupPeriod, backupTime)
+}
+func testAccMysqlVpcConfigBase(name string) string {
+	return fmt.Sprintf(`
+resource "ncloud_vpc" "test_vpc" {
+	name               = "%[1]s"
+	ipv4_cidr_block    = "10.5.0.0/16"
+}
+
+resource "ncloud_subnet" "test_subnet" {
+	vpc_no             = ncloud_vpc.test_vpc.vpc_no
+	name               = "%[1]s"
+	subnet             = "10.5.0.0/24"
+	zone               = "KR-2"
+	network_acl_no     = ncloud_vpc.test_vpc.default_network_acl_no
+	subnet_type        = "PUBLIC"
+}
+`, name)
+}
+func testAccMysqlVpcConfigErrorCaseWhenIsHaSetFalse1(name string) string {
+	return testAccMysqlVpcConfigBase(name) + fmt.Sprintf(`
+resource "ncloud_mysql" "mysql" {
+	subnet_no = ncloud_subnet.test_subnet.id
+	service_name = "%[1]s"
+	name_prefix = "testprefix"
+	user_name = "testusername"
+	user_password = "t123456789!"
+	host_ip = "192.168.0.1"
+	database_name = "test_db"
+
+	is_ha = false
+	is_multi_zone = true
+}
+`, name)
+}
+func testAccMysqlVpcConfigErrorCaseWhenIsHaSetFalse2(name string) string {
+	return testAccMysqlVpcConfigBase(name) + fmt.Sprintf(`
+resource "ncloud_mysql" "mysql" {
+	subnet_no = ncloud_subnet.test_subnet.id
+	service_name = "%[1]s"
+	name_prefix = "testprefix"
+	user_name = "testusername"
+	user_password = "t123456789!"
+	host_ip = "192.168.0.1"
+	database_name = "test_db"
+
+	is_ha = false
+	standby_master_subnet_no = "12346" 
+}
+`, name)
+}
+func testAccMysqlVpcConfigErrorCaseWhenIsHaSetFalse3(name string) string {
+	return testAccMysqlVpcConfigBase(name) + fmt.Sprintf(`
+resource "ncloud_mysql" "mysql" {
+	subnet_no = ncloud_subnet.test_subnet.id
+	service_name = "%[1]s"
+	name_prefix = "testprefix"
+	user_name = "testusername"
+	user_password = "t123456789!"
+	host_ip = "192.168.0.1"
+	database_name = "test_db"
+
+	is_ha = false
+	is_storage_encryption = true 
+}
+`, name)
+}
+func testAccMysqlVpcConfigErrorCaseWhenIsHaSetTrue(name string) string {
+	return testAccMysqlVpcConfigBase(name) + fmt.Sprintf(`
+resource "ncloud_mysql" "mysql" {
+	subnet_no = ncloud_subnet.test_subnet.id
+	service_name = "%[1]s"
+	name_prefix = "testprefix"
+	user_name = "testusername"
+	user_password = "t123456789!"
+	host_ip = "192.168.0.1"
+	database_name = "test_db"
+
+	is_ha = true
+	is_backup = false
+}
+`, name)
+}
+
+func testAccMysqlVpcConfigErrorCaseWhenAutomaticBackup(name string) string {
+	return testAccMysqlVpcConfigBase(name) + fmt.Sprintf(`
+resource "ncloud_mysql" "mysql" {
+	subnet_no = ncloud_subnet.test_subnet.id
+	service_name = "%[1]s"
+	name_prefix = "testprefix"
+	user_name = "testusername"
+	user_password = "t123456789!"
+	host_ip = "192.168.0.1"
+	database_name = "test_db"
+
+	is_backup = true
+	is_automatic_backup = true
+	backup_time = "11:11"
+}
+`, name)
+}
+func testAccMysqlVpcConfigErrorCaseWhenEmptyBackupTime(name string) string {
+	return testAccMysqlVpcConfigBase(name) + fmt.Sprintf(`
+resource "ncloud_mysql" "mysql" {
+	subnet_no = ncloud_subnet.test_subnet.id
+	service_name = "%[1]s"
+	name_prefix = "testprefix"
+	user_name = "testusername"
+	user_password = "t123456789!"
+	host_ip = "192.168.0.1"
+	database_name = "test_db"
+
+	is_backup = true
+	is_automatic_backup = false
+}
+`, name)
 }
