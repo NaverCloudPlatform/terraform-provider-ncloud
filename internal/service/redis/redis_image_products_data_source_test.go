cc68c0fe951eed7105830334a58aa8b5cb86ce3e
youngmn <youngmn82@nate.com>
2024-01-10 14:53:24 +0900

    fix: Change AccTest config from lint errors

--- internal/service/redis/redis_image_products_data_source_test.go
+++ internal/service/redis/redis_image_products_data_source_test.go
@@ -9,28 +9,29 @@ import (
 )
 
 func TestAccDataSourceNcloudRedisImageProducts_vpc_basic(t *testing.T) {
+	productType := "LINUX"
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { acctest.TestAccPreCheck(t) },
 		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDataSourceNcloudRedisImageProductsConfig(),
+				Config: testAccDataSourceNcloudRedisImageProductsConfig(productType),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.ncloud_redis_image_products.all", "image_product_list.0.product_type", "LINUX"),
+					resource.TestCheckResourceAttr("data.ncloud_redis_image_products.all", "image_product_list.0.product_type", productType),
 				),
 			},
 		},
 	})
 }
 
-func testAccDataSourceNcloudRedisImageProductsConfig() string {
+func testAccDataSourceNcloudRedisImageProductsConfig(productType string) string {
 	return fmt.Sprintf(`
 data "ncloud_redis_image_products" "all" {
 	filter {
 			name = "product_type"
-			values = ["LINUX"]
+			values = ["%[1]s"]
 	}
 }
-`)
+`, productType)
 }
