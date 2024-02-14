dd6b4fff5996d7da6f53a278bd72833f607a7307
jungchanSon <rnrmfjc@gmail.com>
2024-02-14 19:41:56 +0900

    test(hadoop): Update hadoop_products_data_source_test.go
     change image product code
     drop product code and add product type
     add `filter` in test case

--- internal/service/hadoop/hadoop_products_data_source_test.go
+++ internal/service/hadoop/hadoop_products_data_source_test.go
@@ -10,32 +10,36 @@ import (
 )
 
 func TestAccDataSourceNcloudHadoopProducts_basic(t *testing.T) {
-	imageProductCode := "SW.VCHDP.LNX64.CNTOS.0708.HDP.15.B050"
-	productCode := "SVR.VCHDP.EDGND.HIMEM.C004.M032.NET.HDD.B050.G002"
+	imageProductCode := "SW.VCHDP.LNX64.CNTOS.0708.HDP.21.B050"
 	infraResourceDetailTypeCode := "EDGND"
+	productType := "STAND"
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { TestAccPreCheck(t) },
 		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDataSourceHadoopProductsConfig(imageProductCode, productCode, infraResourceDetailTypeCode),
+				Config: testAccDataSourceHadoopProductsConfig(imageProductCode, infraResourceDetailTypeCode, productType),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("data.ncloud_hadoop_products.products", "product_list.0.infra_resource_type", "VCHDP"),
-					resource.TestCheckResourceAttr("data.ncloud_hadoop_products.products", "product_list.0.product_code", productCode),
 					resource.TestCheckResourceAttr("data.ncloud_hadoop_products.products", "product_list.0.infra_resource_detail_type", infraResourceDetailTypeCode),
+					resource.TestCheckResourceAttr("data.ncloud_hadoop_products.products", "product_list.0.product_type", productType),
 				),
 			},
 		},
 	})
 }
 
-func testAccDataSourceHadoopProductsConfig(imageProductCode, productCode, infraResourceDetailTypeCode string) string {
+func testAccDataSourceHadoopProductsConfig(imageProductCode, infraResourceDetailTypeCode, productType string) string {
 	return fmt.Sprintf(`
 data "ncloud_hadoop_products" "products" {
 	image_product_code = "%[1]s"
-	product_code = "%[2]s"
-	infra_resource_detail_type_code = "%[3]s"
+	infra_resource_detail_type_code = "%[2]s"
+	
+	filter {
+		name = "product_type"
+		values = ["%[3]s"]
+	}
 }
-`, imageProductCode, productCode, infraResourceDetailTypeCode)
+`, imageProductCode, infraResourceDetailTypeCode, productType)
 }
