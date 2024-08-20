6527e6178c18c0ebd36c8331542a08989962f590
Geun-Oh <kandy1002@naver.com>
2024-08-20 15:14:17 +0900

    feat: Add examples of object storage

---
+++ examples/object_storage/main.tf
@@ -0,0 +1,26 @@
+provider "ncloud" {
+    support_vpc = true
+    access_key = var.access_key
+    secret_key = var.secret_key
+    region = var.region
+}
+
+resource "ncloud_objectstorage_bucket" "bucket" {
+    bucket_name             = "bucket"
+}
+
+resource "ncloud_objectstorage_object" "object" {
+    bucket                  = ncloud_objectstorage.bucket.bucket_name
+    key                     = "hello.md"
+    source                  = "path/to/file"
+}
+
+resource "ncloud_objectstorage_bucket_acl" "bucket_acl" {
+    bucket_id               = ncloud_objectstorage.bucket.id
+    rule                    = "private"
+}
+
+resource "ncloud_objectstorage_object_acl" "object_acl" {
+    object_id               = ncloud_objectstorage.object.id
+    rule                    = "public-read-write"
+}
---
+++ examples/object_storage/variables.tf
@@ -0,0 +1,11 @@
+variable "access_key" {
+  description = "access_key, provide through environment variables"
+}
+
+variable "secret_key" {
+  description = "secret_key, provide through environment variables"
+}
+
+variable "region" {
+  default = "KR"
+}
