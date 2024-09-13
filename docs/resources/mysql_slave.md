50126a1b2e991c418368a25cfdf806b2aad5ed8f
dusdjhyeon <dusdj0813@gmail.com>
2024-09-13 10:54:39 +0900

    feat: add mysql slave resource

---
+++ docs/resources/mysql_slave.md
@@ -0,0 +1,74 @@
+---
+subcategory: "MySQL"
+---
+
+# Resource: ncloud_mysql_slave
+
+Provides a MySQL instance resource.
+
+~> **NOTE:** This resource only supports VPC environment.
+
+## Example Usage
+
+```terraform
+resource "ncloud_vpc" "test_vpc" {
+	name             = "%[1]s"
+	ipv4_cidr_block  = "10.5.0.0/16"
+}
+
+resource "ncloud_subnet" "test_subnet" {
+	vpc_no             = ncloud_vpc.test_vpc.vpc_no
+	name               = "%[1]s"
+	subnet             = "10.5.0.0/24"
+	zone               = "KR-2"
+	network_acl_no     = ncloud_vpc.test_vpc.default_network_acl_no
+	subnet_type        = "PUBLIC"
+}
+
+resource "ncloud_mysql" "mysql" {
+	subnet_no = data.ncloud_subnet.test_subnet.id
+	service_name = "%[1]s"
+	server_name_prefix = "testprefix"
+	user_name = "testusername"
+	user_password = "t123456789!a"
+	host_ip = "192.168.0.1"
+	database_name = "test_db"
+}
+
+resource "ncloud_mysql_slave" "mysql_slave" {
+	mysql_instance_no = ncloud_mysql.mysql.id
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `mysql_instance_no` - (Required) the ID of the associated Mysql Instance.
+* `subnet_no` - (Optional, Required if `is_multi_zone` of MySQL Instance is true) The ID of the associate Subnet.
+
+## Attribute Reference
+
+In addition to all arguments above, the following attributes are exported
+
+* `id` - MySQL Slave Server Instance Number.
+
+# Import
+
+### `terraform import` command
+
+* MySQL Slave can be imported using the `id`. For example:
+```console
+$ terraform import ncloud_mysql_slave.rsc_name 12345
+```
+
+### `import` block
+
+* In terraform v1.5.0 and later, use an [`import` block](https://developer.hashicorp.com/terraform/language/import) to import MySQL Slave using the `id`. For example:
+
+```terraform
+import {
+    to = ncloud_mysql_slave.rsc_name
+    id = "12345"
+}
+```
---
+++ internal/service/mysql/mysql_slave_test.go
@@ -0,0 +1,111 @@
+package mysql_test
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/NaverCloudPlatform/ncloud-sdk-go-v2/services/vmysql"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	. "github.com/terraform-providers/terraform-provider-ncloud/internal/acctest"
+	"github.com/terraform-providers/terraform-provider-ncloud/internal/conn"
+	mysqlservice "github.com/terraform-providers/terraform-provider-ncloud/internal/service/mysql"
+)
+
+func TestAccrResourceNcloudMysqlSlave_vpc_basic(t *testing.T) {
+	var mysqlServerInstance vmysql.CloudMysqlServerInstance
+	testName := fmt.Sprintf("tf-mysqlsv-%s", acctest.RandString(5))
+	resourceName := "ncloud_mysql_slave.mysql_slave"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { TestAccPreCheck(t) },
+		ProtoV6ProviderFactories: ProtoV6ProviderFactories,
+		CheckDestroy:             testAccCheckMysqlSlaveDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccMysqlSlaveConfig(testName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckMysqlSlaveExists(resourceName, &mysqlServerInstance, GetTestProvider(true)),
+					resource.TestCheckResourceAttrSet(resourceName, "mysql_instance_no"),
+				),
+			},
+		},
+	})
+}
+
+func testAccMysqlSlaveConfig(testName string) string {
+	return fmt.Sprintf(`
+data "ncloud_vpc" "test_vpc" {
+	id = "75658"
+}
+
+data "ncloud_subnet" "test_subnet" {
+	id = "172709"
+}
+
+resource "ncloud_mysql" "mysql" {
+	subnet_no = data.ncloud_subnet.test_subnet.id
+	service_name = "%[1]s"
+	server_name_prefix = "testprefix"
+	user_name = "testusername"
+	user_password = "t123456789!a"
+	host_ip = "192.168.0.1"
+	database_name = "test_db"
+}
+
+resource "ncloud_mysql_slave" "mysql_slave" {
+	mysql_instance_no = ncloud_mysql.mysql.id
+}
+`, testName)
+}
+
+func testAccCheckMysqlSlaveExists(n string, slave *vmysql.CloudMysqlServerInstance, provider *schema.Provider) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		resource, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("not found %s", n)
+		}
+
+		if resource.Primary.ID == "" {
+			return fmt.Errorf("no ID is set")
+		}
+
+		config := provider.Meta().(*conn.ProviderConfig)
+		mysqlSlave, err := mysqlservice.GetMysqlSlave(context.Background(), config, resource.Primary.Attributes["mysql_instance_no"])
+		if err != nil {
+			return err
+		}
+
+		if mysqlSlave != nil {
+			*slave = *mysqlSlave
+			return nil
+		}
+
+		return fmt.Errorf("mysql slave not found")
+	}
+}
+
+func testAccCheckMysqlSlaveDestroy(s *terraform.State) error {
+	config := GetTestProvider(true).Meta().(*conn.ProviderConfig)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "ncloud_mysql_slave" {
+			continue
+		}
+		instance, err := mysqlservice.GetMysqlSlave(context.Background(), config, rs.Primary.Attributes["mysql_instance_no"])
+		if err != nil && !strings.Contains(err.Error(), "5001017") {
+			return nil
+		}
+
+		if instance != nil {
+			return errors.New("mysql slave still exists")
+		}
+	}
+
+	return nil
+}
