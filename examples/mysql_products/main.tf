427c654389217bb0ee8a7570ba54fe27d4c9e1a2
dusdjhyeon <dusdj0813@gmail.com>
2024-11-05 17:18:07 +0900

    docs: update example image product code

--- examples/mysql_products/main.tf
+++ examples/mysql_products/main.tf
@@ -5,7 +5,7 @@ provider "ncloud" {
 }
 
 data "ncloud_mysql_products" "all" {
-  image_product_code = "SW.VDBAS.DBAAS.LNX64.CNTOS.0708.MYSQL.8025.B050"
+  image_product_code = "SW.VMYSL.OS.LNX64.ROCKY.0810.MYSQL.B050"
   output_file = "mysql_products.json"
 }
 
