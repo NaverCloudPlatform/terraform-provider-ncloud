af3ef398b25a26040d58d097808b13cc33a9870c
youngmn <youngmn82@nate.com>
2025-02-20 10:30:43 +0900

    feat(apigw): Modfiy acctest

--- internal/service/apigw/product_test.go
+++ internal/service/apigw/product_test.go
@@ -6,12 +6,13 @@ import (
 	"os"
 	"testing"
 
-	"github.com/NaverCloudPlatform/terraform-codegen-poc/internal/ncloudsdk"
-	"github.com/NaverCloudPlatform/terraform-codegen-poc/internal/test"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+
+	. "github.com/terraform-providers/terraform-provider-ncloud/internal/acctest"
+	"github.com/terraform-providers/terraform-provider-ncloud/internal/ncloudsdk"
 )
 
 func TestAccResourceNcloudApigw_product_basic(t *testing.T) {
@@ -20,14 +21,14 @@ func TestAccResourceNcloudApigw_product_basic(t *testing.T) {
 	resourceName := "ncloud_apigw_product.testing_product"
 
 	resource.Test(t, resource.TestCase{
-		PreCheck:                 func() { test.TestAccPreCheck(t) },
-		ProtoV6ProviderFactories: test.ProtoV6ProviderFactories,
+		PreCheck:                 func() { TestAccPreCheck(t) },
+		ProtoV6ProviderFactories: ProtoV6ProviderFactories,
 		CheckDestroy:             testAccCheckProductDestroy,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccproductConfig(productName),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					testAccCheckproductExists(resourceName, test.GetTestProvider(true)),
+					testAccCheckproductExists(resourceName, GetTestProvider(true)),
 					resource.TestCheckResourceAttr(resourceName, "product_name", productName),
 					// check all the other attributes
 				),
@@ -49,9 +50,9 @@ func testAccCheckproductExists(n string, provider *schema.Provider) resource.Tes
 
 		c := ncloudsdk.NewClient("https://apigateway.apigw.ntruss.com/api/v1", os.Getenv("NCLOUD_ACCESS_KEY"), os.Getenv("NCLOUD_SECRET_KEY"))
 
-		response, err := c.GETProductsProductid_TF(context.Background(), &ncloudsdk.PrimitiveGETProductsProductidRequest{
+		response, err := c.GETProductsProductid(context.Background(), &ncloudsdk.GETProductsProductidRequestQuery{
 			// change value with "resource.Primary.ID"
-			Productid: resource.Primary.Attributes["productid"],
+			Productid: &resource.Primary.ID,
 		})
 		if response == nil {
 			return err
@@ -71,9 +72,9 @@ func testAccCheckProductDestroy(s *terraform.State) error {
 		}
 
 		c := ncloudsdk.NewClient("https://apigateway.apigw.ntruss.com/api/v1", os.Getenv("NCLOUD_ACCESS_KEY"), os.Getenv("NCLOUD_SECRET_KEY"))
-		_, err := c.GETProductsProductid_TF(context.Background(), &ncloudsdk.PrimitiveGETProductsProductidRequest{
+		_, err := c.GETProductsProductid(context.Background(), &ncloudsdk.GETProductsProductidRequestQuery{
 			// change value with "rs.Primary.ID"
-			Productid: rs.Primary.Attributes["productid"],
+			Productid: &rs.Primary.ID,
 		})
 		if err != nil {
 			return nil
@@ -86,7 +87,7 @@ func testAccCheckProductDestroy(s *terraform.State) error {
 func testAccproductConfig(productName string) string {
 	return fmt.Sprintf(`
 	resource "ncloud_apigw_product" "testing_product" {
-				product_name = "tf-6uaxt"
+		product_name = "%[1]s"
 		subscription_code = "tf-swm90"
 
 	}`, productName)
