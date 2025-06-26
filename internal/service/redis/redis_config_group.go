18f802dc8b0c2e2fe89cb1a47733637dcceb4bde
yngmn.lee <yngmn.lee@navercorp.com>
2025-06-26 17:27:33 +0900

    remove(cache): Remove VPC config

--- internal/service/redis/redis_config_group.go
+++ internal/service/redis/redis_config_group.go
@@ -96,14 +96,6 @@ func (r *redisConfigGroupResource) Configure(_ context.Context, req resource.Con
 func (r *redisConfigGroupResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
 	var plan redisConfigGroupResourceModel
 
-	if !r.config.SupportVPC {
-		resp.Diagnostics.AddError(
-			"NOT SUPPORT CLASSIC",
-			"resource does not support CLASSIC. only VPC.",
-		)
-		return
-	}
-
 	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
 	if resp.Diagnostics.HasError() {
 		return
